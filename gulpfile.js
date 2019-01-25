184df1dc1bd2f6b159c1d546b292d8c4c6bd6e7e
Andrey Kuznetsov <and.rey.q@yandex.ru>
2019-01-26 00:00:49 +0300

    update gulp

---
+++ gulpfile.js
@@ -0,0 +1,142 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+var path = {
+    public_html: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html: 'public_html/',
+        js: 'public_html/js/',
+        css: 'public_html/css/',
+        img: 'public_html/img/',
+        fonts: 'public_html/fonts/'
+    },
+    src: { //Пути откуда брать исходники
+        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: 'src/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+        css: 'src/css/main.scss',
+        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        css: 'src/css/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './public_html'
+};
+
+
+var config = {
+    server: {
+        baseDir: "./public_html"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_Devil"
+};
+
+
+gulp.task('html', function () {
+    return gulp.src(path.src.html) //Выберем файлы по нужному пути
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(gulp.dest(path.public_html.html)) //Выплюнем их в папку public_html
+        .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
+});
+
+
+gulp.task('js', function () {
+    return gulp.src(path.src.js) //Найдем наш main файл
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(sourcemaps.init()) //Инициализируем sourcemap
+        .pipe(uglify()) //Сожмем наш js
+        .pipe(sourcemaps.write()) //Пропишем карты
+        .pipe(gulp.dest(path.public_html.js)) //Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); //И перезагрузим сервер
+});
+
+
+gulp.task('css', function () {
+    return gulp.src(path.src.css) //Выберем наш main.scss
+        .pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(sass()) //Скомпилируем
+        .pipe(prefixer()) //Добавим вендорные префиксы
+        .pipe(cssmin()) //Сожмем
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.public_html.css)) //И в build
+        .pipe(reload({stream: true}));
+});
+
+
+gulp.task('image', function () {
+    return gulp.src(path.src.img) //Выберем наши картинки
+        .pipe(imagemin({ //Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.public_html.img)) //И бросим в build
+        .pipe(reload({stream: true}));
+});
+
+
+gulp.task('fonts', function() {
+    return gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.public_html.fonts))
+});
+
+
+gulp.task('build', gulp.series(
+    'html',
+    'js',
+    'css',
+    'fonts',
+    'image'
+));
+
+
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html');
+    });
+    watch([path.watch.css], function(event, cb) {
+        gulp.start('css');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts');
+    });
+});
+
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+
+gulp.task('clean', function (cb) {
+    return rimraf(path.clean, cb);
+});
+
+
+gulp.task('default', gulp.series('build', 'webserver', 'watch'));
